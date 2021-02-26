d13a75d843f39fc219eb6558d1c6910bd17130f4
Julien Bidoret – accentgrave <julien@accentgrave.net>
2021-02-26 13:52:21 +0100

    Auto deploy script

---
+++ deploy.php
@@ -0,0 +1,55 @@
+<?php
+	/**
+	 * GIT DEPLOYMENT SCRIPT
+	 *
+	 * Used for automatically deploying websites via github or bitbucket, more deets here:
+	 *
+	 *		https://gist.github.com/1809044
+	 */
+
+	// The commands
+	$commands = array(
+		'echo $PWD',
+		'whoami',
+		'git pull',
+		'git status',
+		'git submodule sync',
+		'git submodule update',
+		'git submodule status',
+	);
+
+	// Run the commands for output
+	$output = '';
+	foreach($commands AS $command){
+		// Run it
+		$tmp = shell_exec($command);
+		// Output
+		$output .= "\n<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #000;\">{$command}\n</span>";
+		$output .= htmlentities(trim($tmp)) . "\n";
+	}
+
+	// Make it pretty for manual user access (and why not?)
+?>
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>./deploy</title>
+	<style>
+		body{
+			margin:2vw 4vw;
+			color:#999
+		}
+		pre{
+			font-size:1rem
+			font-family: Consolas, "Andale Mono WT", "Andale Mono", "Lucida Console", "Lucida Sans Typewriter", "DejaVu Sans Mono", "Bitstream Vera Sans Mono", "Liberation Mono", "Nimbus Mono L", Monaco, "Courier New", Courier, monospace;
+			line-height:1.3
+		}
+	</style>
+</head>
+<body>
+<pre>
+<?php echo $output; ?>
+</pre>
+</body>
+</html>
