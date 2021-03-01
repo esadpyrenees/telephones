df65cde609c26300774d09738a00670348c318f4
Julien Bidoret – accentgrave <julien@accentgrave.net>
2021-03-01 17:08:57 +0100

    Automatisation filtres

--- site/templates/bibliographie.php
+++ site/templates/bibliographie.php
@@ -6,15 +6,10 @@
 
     <section class="filtre">
       <select class="filters-select">
-        <option value="*">Tout</option>
-        <option value=".anais-guilet-corinne-melin">Introduction</option>
-        <option value=".anais-guilet">Quelques spécificités du smartphone à l'écran: une introduction</option>
-        <option value=".martine-beugnet">Cinéma et petits écrans : le cadre et la (dé)mesure</option>
-        <option value=".ghislaine-chabert">Usages du smartphone et grand écran, l’expérience spectatorielle du ‘second screen’</option>
-        <option value=".corinne-melin">Le filmeur au téléphone-caméra dans le cinéma contemporain</option>
-        <option value=".fabien-zocco">Divers exemples d’usage du smartphone dans le champ de l’art contemporain et du cinéma</option>
-        <option value=".laurence-allard">D’une boucle l’autre, TikTok et l’algo-ritournelle : performer entre rage et ennui en temps de pandémie</option>
-        <option value=".raymond-delambre">Vous avez demandé (la police) Ethan Hunt, ne quittez pas : “Fallout”</option>
+        <option value="*">Tout</option>        
+        <?php foreach(page('sommaire')->children()->listed() as $article) :?>
+          <option value="<?= $article->author()->slug() ?>"><?= $article->title() ?></option>
+        <?php endforeach ?>
       </select>
     </section>
 
