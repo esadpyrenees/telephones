f7a77cb08e3fbf8e62c319040c1788937ab4e4bc
normative <cesar.helionjoly@gmail.com>
2021-03-16 18:22:01 +0100

    ajout fichier js Delambre

---
+++ assets/js/delambre.js
@@ -0,0 +1,39 @@
+$(function(){
+	const contentDelambre = $('.content-delambre');
+
+	if (contentDelambre) {
+		createObserver();
+	}
+	
+	function createObserver() {
+	  const container = document.querySelector('#content')
+	  const boxElement = container.querySelectorAll('p');
+	  const boxElementTitre = container.querySelectorAll('h2');
+
+	  var observer;
+	  const options = {
+	    root: null,
+	    rootMargin: "0px",
+	    threshold: 0.5
+	  };
+
+	  boxElement.forEach(function(element) { 
+	  	observer = new IntersectionObserver(handleIntersect , options);
+	  	observer.observe(element);
+	  });
+	  boxElementTitre.forEach(function(elementTitre) { 
+	  	observer = new IntersectionObserver(handleIntersect , options);
+	  	observer.observe(elementTitre);
+	  });
+	}
+
+	function handleIntersect(entries) {
+	  entries.forEach(function(entry) {
+	    if (entry.isIntersecting) {
+	    	entry.target.style.color = "pink";
+		} else {
+			entry.target.style.color = "inherit";
+		}
+	  });
+	}
+})
