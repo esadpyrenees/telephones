8f4ed7489eb45c60d7913f18eaca1aff971c48ae
Christophe Léon <67395386+leon-christophe@users.noreply.github.com>
2021-04-07 10:46:12 +0200

    Référencement et méta données
    On continue de voir ce que ça donne

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -4,13 +4,18 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width,initial-scale=1.0">
 
+        <!-- Primary Meta Tags -->
         <title><?= $site->title() ?> | <?= $page->title() ?></title>
+        <meta name="title" content="<?= $site->title() ?> | <?= $page->title() ?>">
+        <meta name="description" content="<?= $site->seoDescription() ?>">
 
+        <!-- Open Graph / Facebook  -->
         <meta property="og:title" content="<?= $site->title() ?> | <?= $page->title() ?>" />
         <meta property="og:type" content="website" />
         <meta property="og:description" content="<?= $site->seoDescription() ?>" />
         <meta property="og:image" content="<?= url('assets/statics/image-card.jpg') ?>" />
 
+        <!-- Twitter -->
         <meta name="twitter:card" content="summary_large_image">
         <meta name="twitter:site" content="">
         <meta name="twitter:creator" content="">
